00e86e2cddb71ed6327cd5a944ddbf38698fa369
ubadineke <ubadinekeprince@gmail.com>
2024-07-23 17:12:47 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+// Shadowing
+// fn main() {
+//     let x = 5;
+//     println!("THe value of x is {}", x);
+//     let x = 6;
+//     println!("the value of x is {}", x);
+
+//     // const SUBSCRIBER_COUNT: u32 = 100_000;
+
+// }
+
+//Functions 
+fn main(){
+    let sum = checker(3,5);
+    println!("{}",sum)
+}
+
+fn checker (x:i32, y:i32) -> i32{
+x * y
+}
+
+
+//loops
